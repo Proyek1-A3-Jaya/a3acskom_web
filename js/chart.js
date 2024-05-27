159c49aacd024ce5fbabf343702b8860a2edf2f9
WeirdoKitten <satriapermata12@gmail.com>
2024-05-28 03:04:41 +0700

    nambah fitur sort ascending/descending

--- js/chart.js
+++ js/chart.js
@@ -1,29 +1,27 @@
 let allData = {};
 
-function fetchDataAndCreateChart(url, chartId, chartLabel, sliderId, labelId, dataKey) {
+function fetchDataAndCreateChart(url, chartId, chartLabel, sliderId, labelId, orderId, dataKey) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      allData[dataKey] = data; // Menyimpan data pada objek allData dengan key yang sesuai
-      createChart(data, chartId, chartLabel, sliderId, labelId, dataKey);
+      allData[dataKey] = data;
+      createChart(data, chartId, chartLabel, sliderId, labelId, orderId, dataKey);
 
-      // Mengatur nilai min, max, dan value slider sesuai dengan jumlah merek dalam data
       const numBrands = data.length;
       const slider = document.getElementById(sliderId);
       slider.min = 1;
       slider.max = numBrands;
-      slider.value = 10; // Set nilai awal slider ke 10
+      slider.value = 10;
       
-      // Mengatur label slider sesuai dengan nilai awal slider
       const label = document.getElementById(labelId);
       label.textContent = `Jumlah merek yang ditampilkan: 10`;
     })
     .catch(error => console.error('Error:', error));
 }
 
-function createChart(data, chartId, chartLabel, sliderId, labelId, dataKey) {
+function createChart(data, chartId, chartLabel, sliderId, labelId, orderId, dataKey) {
   const sortedData = data.sort((a, b) => b.harga_rata_rata - a.harga_rata_rata);
-  const initialData = sortedData.slice(0, 10); // Menampilkan hanya 10 merek pertama
+  const initialData = sortedData.slice(0, 10);
 
   const labels = initialData.map(item => item.merek);
   const chartData = initialData.map(item => item.harga_rata_rata);
@@ -42,7 +40,7 @@ function createChart(data, chartId, chartLabel, sliderId, labelId, dataKey) {
       }]
     },
     options: {
-      indexAxis: 'y', // Mengubah chart menjadi horizontal
+      indexAxis: 'y',
       scales: {
         x: {
           beginAtZero: true,
@@ -63,13 +61,23 @@ function createChart(data, chartId, chartLabel, sliderId, labelId, dataKey) {
 
   const filterSlider = document.getElementById(sliderId);
   const filterLabel = document.getElementById(labelId);
+  const orderSelect = document.getElementById(orderId);
 
-  filterSlider.addEventListener('input', function() {
-    const numBrands = this.value;
+  function updateData() {
+    const numBrands = filterSlider.value;
+    const order = orderSelect.value;
     filterLabel.textContent = `Jumlah merek yang ditampilkan: ${numBrands}`;
+
+    const sortedData = order === 'ascending' ?
+      allData[dataKey].sort((a, b) => a.harga_rata_rata - b.harga_rata_rata) :
+      allData[dataKey].sort((a, b) => b.harga_rata_rata - a.harga_rata_rata);
+
     const filteredData = sortedData.slice(0, numBrands);
     updateChart(chart, filteredData);
-  });
+  }
+
+  filterSlider.addEventListener('input', updateData);
+  orderSelect.addEventListener('change', updateData);
 }
 
 function updateChart(chart, data) {
@@ -82,10 +90,10 @@ function updateChart(chart, data) {
 }
 
 // Fetch data and create charts for all datasets
-fetchDataAndCreateChart('dataScrap/headphone_appr.json', 'headphoneChart', 'Harga Rata-Rata Headphone', 'headphoneFilterSlider', 'headphoneFilterLabel', 'headphone');
-fetchDataAndCreateChart('dataScrap/keyboard_appr.json', 'keyboardChart', 'Harga Rata-Rata Keyboard', 'keyboardFilterSlider', 'keyboardFilterLabel', 'keyboard');
-fetchDataAndCreateChart('dataScrap/monitor_appr.json', 'monitorChart', 'Harga Rata-Rata Monitor', 'monitorFilterSlider', 'monitorFilterLabel', 'monitor');
-fetchDataAndCreateChart('dataScrap/mouse_appr.json', 'mouseChart', 'Harga Rata-Rata Mouse', 'mouseFilterSlider', 'mouseFilterLabel', 'mouse');
-fetchDataAndCreateChart('dataScrap/speaker_appr.json', 'speakerChart', 'Harga Rata-Rata Speaker', 'speakerFilterSlider', 'speakerFilterLabel', 'speaker');
-fetchDataAndCreateChart('dataScrap/tas_laptop_appr.json', 'tasLaptopChart', 'Harga Rata-Rata Tas Laptop', 'tasLaptopFilterSlider', 'tasLaptopFilterLabel', 'tas_laptop');
-fetchDataAndCreateChart('dataScrap/webcam_appr.json', 'webcamChart', 'Harga Rata-Rata Webcam', 'webcamFilterSlider', 'webcamFilterLabel', 'webcam');
+fetchDataAndCreateChart('dataScrap/headphone_appr.json', 'headphoneChart', 'Harga Rata-Rata Headphone', 'headphoneFilterSlider', 'headphoneFilterLabel', 'headphoneOrderSelect', 'headphone');
+fetchDataAndCreateChart('dataScrap/keyboard_appr.json', 'keyboardChart', 'Harga Rata-Rata Keyboard', 'keyboardFilterSlider', 'keyboardFilterLabel', 'keyboardOrderSelect', 'keyboard');
+fetchDataAndCreateChart('dataScrap/monitor_appr.json', 'monitorChart', 'Harga Rata-Rata Monitor', 'monitorFilterSlider', 'monitorFilterLabel', 'monitorOrderSelect', 'monitor');
+fetchDataAndCreateChart('dataScrap/mouse_appr.json', 'mouseChart', 'Harga Rata-Rata Mouse', 'mouseFilterSlider', 'mouseFilterLabel', 'mouseOrderSelect', 'mouse');
+fetchDataAndCreateChart('dataScrap/speaker_appr.json', 'speakerChart', 'Harga Rata-Rata Speaker', 'speakerFilterSlider', 'speakerFilterLabel', 'speakerOrderSelect', 'speaker');
+fetchDataAndCreateChart('dataScrap/tas_laptop_appr.json', 'tasLaptopChart', 'Harga Rata-Rata Tas Laptop', 'tasLaptopFilterSlider', 'tasLaptopFilterLabel', 'tasLaptopOrderSelect', 'tas_laptop');
+fetchDataAndCreateChart('dataScrap/webcam_appr.json', 'webcamChart', 'Harga Rata-Rata Webcam', 'webcamFilterSlider', 'webcamFilterLabel', 'webcamOrderSelect', 'webcam');
